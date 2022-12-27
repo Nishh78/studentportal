d5c3d3ee69a0dc8f913e45ff7cec5eb8019b8f88
Nishit dev <ravalnishit81@gmail.com>
2022-12-28 02:00:26 +0530

    url changes

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ app.use('/api/v1/', router);
 const PORT = process.env.PORT || 8000;
 
 // if (process.env.NODE_ENV === 'production') {
-app.use(express.static('frontend/build'));
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+	res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
 });
 // }
 app.listen(PORT, () => {
