d15136448070cebd5df81c6014ec0036ce81ba6a
Nishit dev <ravalnishit81@gmail.com>
2022-12-28 23:29:05 +0530

    route updated

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-// simple route
-app.get("/", (req, res) => {
-	res.json({ message: "Welcome to student application." });
-});
+// // simple route
+// app.get("/", (req, res) => {
+// 	res.json({ message: "Welcome to student application." });
+// });
 
 app.use('/api/v1/', router);
 
